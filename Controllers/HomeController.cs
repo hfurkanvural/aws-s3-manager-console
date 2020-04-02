b10568dcae939a3a817ebea418c4e8529ebff64d
hfurkanvural <hfurkanvural@gmail.com>
2020-04-03 00:37:17 +0300

    Bucket listing and object adding added.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;  
+using System.Web; 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Amazon;  
+using Amazon.Runtime;  
+using Amazon.S3;  
+using Amazon.S3.Model;  
+using Amazon.S3.Transfer; 
+using Microsoft.Extensions.Configuration;
 using s3_example.Models;
 
 namespace s3_example.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public class S3Config
         {
-            _logger = logger;
+            public string bucketName {get; set;}
+            public RegionEndpoint bucketRegion {get; set;} = RegionEndpoint.EUCentral1;
+            public string accesskey {get; set;}
+            public string secretkey {get; set;}
+        }
+        private IConfiguration config;
+        private S3Config s3config;
+        
+
+    
+        public HomeController(IConfiguration configuration)
+        {
+            config = configuration;
+            s3config = config.GetSection("S3Config").Get<S3Config>();
         }
 
         public IActionResult Index()
@@ -23,10 +43,48 @@ namespace s3_example.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]  
+        public async Task<IActionResult> s3Manager()  
+        {  
+            ListObjectsV2Request request = new ListObjectsV2Request{
+                    BucketName = s3config.bucketName,
+                    MaxKeys = 10
+                };
+            ListObjectsV2Response response;
+            
+            var client = new AmazonS3Client(s3config.accesskey, s3config.secretkey, s3config.bucketRegion);
+
+            response = await client.ListObjectsV2Async(request);
+            List<string> images =  new List<string>();
+            foreach (S3Object entry in response.S3Objects)
+            {
+                images.Add("https://hfv-itu-aws-course.s3.eu-central-1.amazonaws.com/"+entry.Key);
+            }
+            return View(images);  
+        }  
+        public async Task<IActionResult> uploadFile (IFormFile file)
         {
-            return View();
+            using (var client = new AmazonS3Client(s3config.accesskey, s3config.secretkey, s3config.bucketRegion))
+            {
+                using (var newMemoryStream = new MemoryStream())
+                {
+                    file.CopyTo(newMemoryStream);
+
+                    var uploadRequest = new TransferUtilityUploadRequest
+                    {
+                        InputStream = newMemoryStream,
+                        Key = file.FileName,
+                        BucketName = s3config.bucketName,
+                        CannedACL = S3CannedACL.PublicRead
+                    };
+
+                    var fileTransferUtility = new TransferUtility(client);
+                    await fileTransferUtility.UploadAsync(uploadRequest);
+                }
+            }
+            return RedirectToAction(nameof(s3Manager));
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
